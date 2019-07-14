23316a25d3c49d57f0fdc3e482ebe07e91a336c0
Mrinal Kothari <mrinalkothari@Mrinals-MacBook-Air.local>
2019-07-14 23:17:50 +0530

    these are the HTML and JS files

---
+++ trying.js
@@ -0,0 +1,5 @@
+var first = prompt("Enter your first name");
+var second = prompt("Enter your second name");
+var age = prompt("Enter the your age");
+console.log("Hey nice to meet you, " + first + " " + second);
+console.log("Your age is " + age);
